30088b066fbf115d9b225dac52419d097a3d0087
RameshBandari26 <bandariramesh133@gmail.com>
2025-06-07 14:24:34 +0530

    updated

--- WEB-INF/classes/db/DBConnection.java
+++ WEB-INF/classes/db/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12783526";
-    private static final String USER = "sql12783526";
-    private static final String PASSWORD = "5na7xrCwsP";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USERNAME");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
